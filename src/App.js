af15a4ac92d51343c4406b3a602ba6c5a537ef0a
J4DER4 <joona.maaranen@aalto.fi>
2024-03-11 23:42:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+class PasswordForm extends React.Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const password = event.target.elements.passwordInput.value;
+    console.log('Password:', password);
+
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="login__form">
+        <div className="login__inputs">
+          <div className="login__box">
+            <input type="password" required className="login__input" id="passwordInput" autoFocus aria-label="password" />
+          </div>
+        </div>
+        <button type="submit" className="login__button">Enter</button>
+      </form>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login_container">
+      <video autoPlay muted loop playsInline className="login__bg">
+        <source src="ezgif.com-video-to-mp4-converter.mp4" type="video/mp4" />
+      </video>
+      <PasswordForm />
     </div>
   );
 }
 
-export default App;
+export default App; 
